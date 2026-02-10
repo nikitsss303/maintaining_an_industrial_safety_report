05b9eb7f8741a8b3ba9ad0591238b58d0079d719
Nikita Smantser <nikita.a.smantser@gmail.com>
2026-02-10 11:27:16 +0300

    refactor: config alembic env file

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app.parse_env.py import POSTGRES_DSN
+from app.config.parse_env import POSTGRES_DSN
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,9 +20,9 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models import test_models
+
+target_metadata = test_models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
